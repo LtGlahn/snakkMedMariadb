75d6a5ed325fed5d9c2d861fcfce1ad01efe11b0
LtGlahn <jensen72@gmail.com>
2023-11-05 00:34:21 +0100

    Henter alle data fra datafangst-kontrakt

--- mariadbpython.py
+++ mariadbpython.py
@@ -27,12 +27,15 @@ def lagCursor( secretsfile='secrets.json', database=None ):
 
     return (conn, cursor)    
 
-def hentFraTabell( tabellNavn:str, cursor, modifikator='LIMIT 10'):
+def hentFraTabell( tabellNavn:str, cursor, modifikator='LIMIT 10', databegrensning=True ):
     """
     Henter data fra angitt tabell. Modifikator kan f.eks være WHERE  - statement eller noe sånt. 
     """
 
     skjema = hentSkjema( tabellNavn, cursor )
+
+    if databegrensning and not 'LIMIT' in modifikator.upper(): 
+        modifikator += ' LIMIT 5000'
 
     query = f"SELECT * from {tabellNavn} {modifikator}"
     cursor.execute( query)
@@ -45,6 +48,53 @@ def hentFraTabell( tabellNavn:str, cursor, modifikator='LIMIT 10'):
 
     return data 
 
+def hentAltFraKontrakt( kontraktId, cursor ): 
+    """
+    Henter alle data tilknyttet en kontrakt 
+    """
+
+    resultat = {
+            'feature_association2' : [],  
+            'feature_attribute2' : [], 
+            'feature_geometry' : [], 
+            'feature_locational2' : [],
+            'feature_locks' : [],
+            'file' : [],
+            }
+
+    resultat['project'] = hentFraTabell( 'project', cursor, modifikator=f"where project_id = {kontraktId} ")
+    resultat['comment'] = hentFraTabell( 'comment', cursor, modifikator=f"where project_id = {kontraktId} ")
+    resultat['contract_change'] = hentFraTabell( 'contract_change', cursor, modifikator=f"where contract_id = {kontraktId} ")
+    resultat['contract_visit'] = hentFraTabell( 'contract_visit', cursor, modifikator=f"where contract_id = {kontraktId} ")
+    resultat['event'] = hentFraTabell( 'event', cursor, modifikator=f"where project_id = {kontraktId} ")
+    resultat['feature2'] = hentFraTabell( 'feature2', cursor, modifikator=f"where project_id = {kontraktId} ")
+    resultat['nvdb_submission'] = hentFraTabell( 'nvdb_submission', cursor, modifikator=f"where project_id = {kontraktId}")
+    resultat['project_locks'] = hentFraTabell( 'project_locks', cursor, modifikator=f"where project_id = {kontraktId}")
+    resultat['project_map_comment'] = hentFraTabell( 'project_map_comment', cursor, modifikator=f"where project_id = {kontraktId}")
+    resultat['project_milestone'] = hentFraTabell( 'project_milestone', cursor, modifikator=f"where project_id = {kontraktId}")
+    resultat['validation_issue2'] = hentFraTabell( 'validation_issue2', cursor, modifikator=f"where project_id = {kontraktId}")
+
+    for feat in resultat['feature2']:
+        relasjoner = hentFraTabell( 'feature_association2', cursor, modifikator=f"where parent_feature_id = {feat['id']} or child_feature_id = {feat['id']} ")
+        resultat['feature_association2'].extend( relasjoner )
+
+        egenskaper = hentFraTabell( 'feature_attribute2', cursor, modifikator=f"where feature_id = {feat['id']}")
+        resultat['feature_attribute2'].extend( egenskaper )
+
+        geometri = hentFraTabell( 'feature_geometry', cursor, modifikator=f"where feature_id = {feat['id']}")
+        resultat['feature_geometry'].extend( geometri )
+
+        stedfesting = hentFraTabell( 'feature_locational2', cursor, modifikator=f"where feature_id = {feat['id']}")
+        resultat['feature_locational2'].extend( stedfesting )
+
+        lock = hentFraTabell( 'feature_locks', cursor, modifikator=f"where feature_id = {feat['id']}")
+        resultat['feature_locks'].extend( lock )
+
+        filer = hentFraTabell( 'file', cursor, modifikator=f"where feature_id = {feat['id']}")
+        resultat['file'].extend( filer )
+
+
+    return resultat
 
 def hentSkjema( tabell:str, cursor ):
     """
